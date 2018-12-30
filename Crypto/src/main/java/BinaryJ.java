01b98f1c663cf75397a1eba514f0c6f93928b8be
Toorero <toorero6@gmail.com>
2018-12-30 03:04:26 +0100

    CRYPTO:
    Package-Name changed to Symbroson.
    Removed java classes

--- Crypto/src/main/java/BinaryJ.java
+++
@@ -1,8 +0,0 @@
-public class BinaryJ {
-
-    static String crypt(String s, String key) {
-        final int keyi = Integer.parseInt(key, 2);
-        return new String(s.chars().map(c -> c ^ keyi).toArray(), 0, s.length());
-    }
-
-}
